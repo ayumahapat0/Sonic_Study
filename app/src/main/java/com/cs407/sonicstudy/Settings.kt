e47a6da93d5b32b13aec47df004cbe67aefa99ea
Ayush Mahapatra <mahapatra3@wisc.edu>
2024-11-21 18:57:03 -0600

    initial Settings options created

--- app/src/main/java/com/cs407/sonicstudy/Settings.kt
+++ app/src/main/java/com/cs407/sonicstudy/Settings.kt
@@ -2,14 +2,21 @@ package com.cs407.sonicstudy
 
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Settings: AppCompatActivity() {
-     override fun onCreate(savedInstanceState: Bundle?) {
+class Settings: AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    private lateinit var deckSpinner: Spinner
+    private lateinit var studySpinner: Spinner
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
@@ -24,6 +31,36 @@ class Settings: AppCompatActivity() {
 //        val resultTextView = findViewById<TextView>(R.id.resultTextView)
 //        resultTextView.text = message
 
+         deckSpinner = findViewById(R.id.numDecks)
+         ArrayAdapter.createFromResource(
+             this,
+             R.array.num_decks,
+             android.R.layout.simple_spinner_item
+         ).also { adapter ->
+             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+             deckSpinner.adapter = adapter
+         }
 
+         studySpinner = findViewById(R.id.studyLimit)
+         ArrayAdapter.createFromResource(
+             this,
+             R.array.study_limit,
+             android.R.layout.simple_spinner_item
+         ).also { adapter ->
+             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+             studySpinner.adapter = adapter
+         }
+
+        deckSpinner.onItemSelectedListener = this
+        studySpinner.onItemSelectedListener = this
+
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        parent?.getItemAtPosition(position)
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        // Do Nothing
     }
 }
